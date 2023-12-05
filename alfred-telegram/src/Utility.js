d8fc45114a38b5431e3ee1b2b00bd202f7cc97ac
Freeze <rain-ilia@rambler.ru>
2023-12-05 20:13:29 +0100

    chunkString()

--- alfred-telegram/src/Utility.js
+++ alfred-telegram/src/Utility.js
@@ -14,4 +14,14 @@ export const loge = (msg, ...details) => {
     console.error(`${msg}.${formattedDetails.length > 0 ? ` args: ${formattedDetails.join(' | ')}` : ''}`);
   }
 }
+
+//Telegram maximum message length is 4096
+export function chunkString(str, chunkSize = 3072){
+	log('Chunking the string', str)
+	const chunks = [];
+  for (let i = 0; i < str.length; i += chunkSize) {
+    chunks.push(str.slice(i, i + chunkSize));
+  }
+  log('Chunking result', chunks)
+	return chunks
 }
