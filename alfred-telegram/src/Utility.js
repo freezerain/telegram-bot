20f421bfb0e7f180a34bb37abfe7fcd5fbccef4a
Freeze <rain-ilia@rambler.ru>
2023-12-05 20:13:18 +0100

    logging improvement

--- alfred-telegram/src/Utility.js
+++ alfred-telegram/src/Utility.js
@@ -4,13 +4,14 @@ const isLoggingError = true;
 export const log = (msg, ...details) => {
   if(isLogging){
     const formattedDetails = details.map((param) => JSON.stringify(param));
-    console.log(`${msg}. ${formattedDetails.join('. ')}`);
+    console.log(`${msg}.${formattedDetails.length > 0 ? ` args: ${formattedDetails.join(' | ')}` : ''}`);
   }
 }
 
 export const loge = (msg, ...details) => {
   if(isLoggingError){
 		const formattedDetails = details.map((param) => JSON.stringify(param));
-    console.error(`${msg}. ${formattedDetails.join('. ')}`);
+    console.error(`${msg}.${formattedDetails.length > 0 ? ` args: ${formattedDetails.join(' | ')}` : ''}`);
   }
+}
 }
