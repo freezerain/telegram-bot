a6ddeb7e29935090f027ac8faf641810ba48e61d
Freeze <rain-ilia@rambler.ru>
2023-12-21 17:54:51 +0100

    [test] fetchApi.test.js cleanup

--- alfred-telegram/test/api/fetchApi.test.js
+++ alfred-telegram/test/api/fetchApi.test.js
@@ -1,79 +1,52 @@
-import { expect } from 'chai';
+import { assert } from 'chai';
 import sinon from 'sinon';
 import { FetchApi } from '#main';
 
 describe('FetchApi', () => {
-	describe('#fetchData', () => {
-		it('should fetch data successfully', async () => {
-			const baseUrl = 'https://example.com/api';
-			const api = new FetchApi(baseUrl);
+	const baseUrl = 'https://example.com/api';
+	const exampleEndpoint = 'exampleEndpoint';
+	let api;
 
-			// Stub the fetch function using Sinon
-			const fetchStub = sinon.stub(globalThis, 'fetch');
-			fetchStub.resolves({ ok: true, json: async () => ({ data: 'test data' }) });
+	beforeEach(() => {
+		api = new FetchApi(baseUrl);
+	});
+	afterEach(() => {
+		sinon.restore();
+	});
 
-			const endpoint = 'exampleEndpoint';
-			const options = { method: 'GET' };
-			const data = await api.fetchData(endpoint, options);
+	it('should fetch data successfully', async () => {
+		const fakeFetch = sinon.fake.resolves({ ok: true, json: async () => ({ data: 'test data' }) });
+		sinon.replace(globalThis, 'fetch', fakeFetch);
+		const data = await api.fetchData(exampleEndpoint, { method: 'GET' });
+		assert.isTrue(fakeFetch.calledWith(`${baseUrl}/${exampleEndpoint}`, { method: 'GET' }));
+		assert.deepEqual(data, { data: 'test data' });
+	});
 
-			// Ensure fetch was called with the correct URL and options
-			const expectedUrl = `${baseUrl}/${endpoint}`;
-			expect(fetchStub.calledWith(expectedUrl, options)).to.be.true;
+	it('should handle fetch error', async () => {
+		const fakeFetch = sinon.fake.rejects(new Error('Fake fetch error'));
+		sinon.replace(globalThis, 'fetch', fakeFetch);
+		try {
+			await api.fetchData(exampleEndpoint, { method: 'GET' });
+			assert.fail('Expected an error but did not receive one.');
+		} catch (error) {
+			assert.equal(error.message, 'Fetch failed');
+		}
+	});
 
-			// Ensure the returned data matches the expected result
-			expect(data).to.deep.equal({ data: 'test data' });
-
-			// Restore the original fetch function
-			fetchStub.restore();
+	it('should handle non-ok response', async () => {
+		const fakeFetch = sinon.fake.resolves({
+			ok: false,
+			status: 404,
+			statusText: 'Not Found',
+			text: async () => 'Not Found'
 		});
-
-		it('should handle fetch error', async () => {
-			const baseUrl = 'https://example.com/api';
-			const api = new FetchApi(baseUrl);
-
-			// Stub the fetch function to simulate an error
-			const fetchStub = sinon.stub(globalThis, 'fetch');
-			fetchStub.rejects(new Error('Fake fetch error'));
-
-			const endpoint = 'exampleEndpoint';
-			const options = { method: 'GET' };
-
-			// Ensure the fetchData method throws the expected error
-			try {
-				await api.fetchData(endpoint, options);
-				// If it doesn't throw an error, fail the test
-				expect.fail('Expected an error but did not receive one.');
-			} catch (error) {
-				expect(error.message).to.equal('Fetch failed');
-			}
-
-			// Restore the original fetch function
-			fetchStub.restore();
-		});
-
-		it('should handle non-ok response', async () => {
-			const baseUrl = 'https://example.com/api';
-			const api = new FetchApi(baseUrl);
-
-			// Stub the fetch function to simulate a non-ok response
-			const fetchStub = sinon.stub(globalThis, 'fetch');
-			fetchStub.resolves({ ok: false, status: 404, statusText: 'Not Found', text: async () => 'Not Found' });
-
-			const endpoint = 'exampleEndpoint';
-			const options = { method: 'GET' };
-
-			// Ensure the fetchData method throws the expected error for non-ok response
-			try {
-				await api.fetchData(endpoint, options);
-				// If it doesn't throw an error, fail the test
-				expect.fail('Expected an error but did not receive one.');
-			} catch (error) {
-				expect(error.message).to.equal('Fetch failed');
-				expect(error.cause.message).to.equal('Response error, status: 404 - Not Found. Not Found');
-			}
-
-			// Restore the original fetch function
-			fetchStub.restore();
-		});
+		sinon.replace(globalThis, 'fetch', fakeFetch);
+		try {
+			await api.fetchData(exampleEndpoint, { method: 'GET' });
+			assert.fail('Expected an error but did not receive one.');
+		} catch (error) {
+			assert.equal(error.message, 'Fetch failed');
+			assert.equal(error.cause.message, 'Response error, status: 404 - Not Found. Not Found');
+		}
 	});
 });
